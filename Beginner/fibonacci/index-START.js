cd0ea7e114e589795263173f61e216c13783f2f2
ma.inespeniche@gmail.com <ma.inespeniche@gmail.com>
2021-02-10 19:44:41 -0500

    Solved by Jose

--- Beginner/fibonacci/index-START.js
+++ Beginner/fibonacci/index-START.js
@@ -6,7 +6,9 @@ where the sequence is:
 
 
 function fibonacci(n) {
-    // Code goes here
+	if (n < 2) return 1;
+	if (n === 2) return 2
+	return fibonacci(n-1) + fibonacci(n-2);
 }
 
 module.exports = fibonacci
